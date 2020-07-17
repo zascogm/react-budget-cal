43e740b7bc0860f101876929190b31e2ad49d8cb
zascogm <zascogm@gmail.com>
2020-07-17 16:02:35 +1000

    add logic for expenseform and expenseitem components

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import {MdSend} from 'react-icons/md'
 
 const ExpenseForm = () => {
   return (
-    <div>
-      hello from expense form
-    </div>
+    <form action="">
+      <div className="form-center">
+        <div className="form-group">
+          <label htmlFor="charge">charge</label>
+          <input 
+            type="text" 
+            className="form-control" 
+            id="charge"
+            name="charge"
+            placeholder="e.g. rent"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">amount</label>
+          <input 
+            type="text" 
+            className="form-control" 
+            id="amount"
+            name="amount"
+            placeholder="e.g. 100"
+          />
+        </div>
+      </div>
+      <button type="submit" className="btn">
+        submit
+        <MdSend className="btn-icon"/>
+      </button>
+    </form>
   )
 }
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import {MdEdit, MdDelete} from 'react-icons/md'
 
-const ExpenseItem = () => {
+const ExpenseItem = ({expense}) => {
+  const {id, charge, amount} = expense;
+  console.log(id);
   return (
-    <div>
-      hello from expense item
-    </div>
-  )
+    <li className="item">
+      <div className="info">
+        <span className="expense">{charge}</span>
+        <span className="amount">${amount}</span>
+      </div>
+      <div>
+        <button className="edit-btn" arial-label="edit button"><MdEdit /></button>
+        <button className="clear-btn" arial-label="delete button"><MdDelete /></button>
+      </div>
+    </li>
+  );
 }
 
 export default ExpenseItem
