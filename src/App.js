2f30f98c8f7b4db1631e98626c0c09adfb83872a
zascogm <zascogm@gmail.com>
2020-07-20 08:24:27 +1000

    add logic for alert component

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   const [expenses, setExpenses] = useState(initialExpenses);
   const [charge, setCharge] = useState('');
   const [amount, setAmount] = useState('');
-  // const [alert,]
+  const [alert, setAlert] = useState({show:false});
 
   const handleCharge = e => {
     setCharge(e.target.value);
@@ -24,22 +24,32 @@ function App() {
   const handleAmount = e => {
     setAmount(e.target.value);
   }
+
+  const handleAlert = ({type, text}) => {
+    setAlert({show: true, type, text});
+    setTimeout(() => {
+      setAlert({show: false});
+    }, 3000);
+  };
+
   
   const handleSubmit = e => {
     e.preventDefault();
 
-    if (charge !== '' && amount > 0) {
+    if (charge !== "" && amount > 0) {
       const singleExpense = {id: uuidv4(), charge, amount};
       setExpenses([...expenses, singleExpense]);
+      handleAlert({type: 'success', text: 'item added'});
       setCharge('');
       setAmount('');
     } else {
-      //call handle alert call
+      handleAlert({type: 'danger', text: `charge can't be empty value and amount has to be bigger than zero`});
     }
   }
 
   return (
     <>
+      {alert.show && <Alert type={alert.type} text={alert.text} />}
       <Alert/>
       <h1>budget calculator</h1>
       <main className="App">
--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Alert = () => {
+const Alert = ({type, text}) => {
   return (
-    <div>
-      hello from Alert
+    <div className={`alert alert-${type}`}>
+      {text}
     </div>
   )
-}
+};
 
-export default Alert
+export default Alert;
